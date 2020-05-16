c6be78f333e3d6755c0cf8e13a4bc2bc14d0b75a
Wade Chang <wadechang@users.noreply.github.com>
2020-05-16 01:43:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,7 @@ sudo apt-get upgrade
 sudo apt-get install git curl llvm lldb clang python-pip meld ctags
 
 # Install Visual Studio Code
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update
-sudo apt-get install code # or code-insiders
+sudo snap install --classic code # or code-insiders
 
 #Install Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
