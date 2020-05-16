55d9be44fe0e2495cc4f794101686af5a7fbbf40
Wade Chang <wadechang@users.noreply.github.com>
2020-05-16 01:40:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,12 +24,6 @@ sudo apt-get install -y nodejs
 # Install Rails 5
 \curl -L https://get.rvm.io | bash -s stable --ruby
 
-# Install Sublime Text 3
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
-
 # Install Go Lang
 sudo apt-get install golang-go
 
